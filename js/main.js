cdc09a3f01b723ca9a3e2cc65f37d150a3a86194
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2021-01-22 00:16:13 -0300

    Creation of variables

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let inputField = document.getElementById('task-register')
+let btnRegister = document.getElementById('thisButton')
+let listTotal = document.getElementById('list')
+
+console.log(inputField,btnRegister,listTotal)
