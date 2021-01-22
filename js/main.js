f105a1eb9210d104c37453250a6cee60e4d82801
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2021-01-22 07:12:17 -0300

    Creation of the array containing the tasks and construction of the function that creates a list and adds the elements

--- js/main.js
+++ js/main.js
@@ -2,4 +2,28 @@ let inputField = document.getElementById('task-register')
 let btnRegister = document.getElementById('thisButton')
 let listTotal = document.getElementById('list')
 
-console.log(inputField,btnRegister,listTotal)
+let tasks = [
+
+    'make a coffe',
+    'make a tea'
+
+]
+
+function renderTasks() {
+
+    for (task of tasks) {
+
+        //Creates the list item
+        let itemList = document.createElement('li')
+
+        //Add classes to the list item
+        itemList.setAttribute('class', 'list-group-item list-group-item-action')
+
+        //
+        let itemText = document.createTextNode(task)
+
+    }
+
+}
+
+renderTasks()
